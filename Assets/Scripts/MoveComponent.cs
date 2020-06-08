62df53a77d923ddd148d70b9b9d5a99dd7afb8be
JPotterf <Jacob.Potterf@gmail.com>
2020-06-08 22:34:05 +0300

    added player/ground movement and obj pooling for ground and enemy prefabs

---
+++ Assets/Scripts/MoveComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MoveComponent : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float objectDistance = -40f;
+    [SerializeField] private float despawnDistance = -110f;
+    private bool canSpawnGround = true;
+
+    private void Update()
+    {
+        transform.position += -transform.forward * speed * Time.deltaTime;
+
+        if (transform.position.z <= objectDistance && transform.tag == "Ground" && canSpawnGround)
+        {
+            ObjectSpawner.instance.SpawnGround();
+            canSpawnGround = false;
+        }
+        if(transform.position.z <= despawnDistance)
+        {
+            canSpawnGround = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
